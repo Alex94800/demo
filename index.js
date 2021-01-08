af728d7e4fb70883f04e41b947b0944c0ff25890
alexandreguedj <alexandreguedj@gmail.com>
2021-01-08 23:29:55 +0100

    change index

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-server.use(router)
 server.all('*', function (req, res, next) {
+  console.log('eh')
   if (req.method === 'GET') {
     next()
   } else {
     res.sendStatus(200)
   }
 })
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
 server.listen(process.env.PORT || 3000, () => {
   console.log('JSON Server is running')
 })
